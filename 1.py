1cdc9e43a50b48c6cf6fca679e5ac45041975ac8
toptunova05 <144706620+toptunova05@users.noreply.github.com>
2023-10-08 23:12:57 +0300

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,9 @@
+import turtle as tr
+from random import *
+tr.color('Red')
+for i in range (100):
+    x=randint(1,400)
+    tr.forward(x)
+    y=randint(1,360)
+    y=randint(0,360)
+    tr.left(y)
